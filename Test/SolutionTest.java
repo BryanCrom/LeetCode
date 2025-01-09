6bf70c5b17d07ecba8586a865ec9e27d2eac9988
Bryan <BryanCrombach@gmail.com>
2025-01-09 15:15:30 +1300

    Delete Node in a BST

--- Test/SolutionTest.java
+++ Test/SolutionTest.java
@@ -10,7 +10,8 @@ class SolutionTest {
     @org.junit.jupiter.api.Test
     void LeetCodeTest() {
         Solution solution = new Solution();
-        TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7));
-        assertEquals(null, solution.searchBST(root, 5));
+        TreeNode root = new TreeNode(5, new TreeNode(3, new TreeNode(2), new TreeNode(4)), new TreeNode(6, null, new TreeNode(7)));
+        TreeNode answer = new TreeNode(5, new TreeNode(4, new TreeNode(2), null), new TreeNode(6, null, new TreeNode(7)));
+        assertEquals(answer, solution.deleteNode(root, 3));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,47 @@
 import java.util.*;
 
 class Solution {
-    public TreeNode searchBST(TreeNode root, int val) {
-        while(root != null) {
-            if (root.val == val) {
-                return root;
+    public TreeNode deleteNode(TreeNode root, int key) {
+
+        //base case if null
+        if (root == null){
+            return null;
+        }
+
+        //traverse to the right if key is greater
+        if(root.val < key){
+            root.right = deleteNode(root.right, key);
+        }
+        //traverse left if key is lesser
+        else if(root.val > key){
+            root.left = deleteNode(root.left, key);
+        }
+        else{
+            //if node to be deleted has 0 or 1 children
+            if(root.left == null){
+                return root.right;
             }
-            else if (root.val < val) {
-                root = root.right;
+            else if(root.right == null){
+                return root.left;
             }
-            else {
-                root = root.left;
+
+            //if node to be deleted has 2 children
+            else{
+
+                //find in-order successor
+                TreeNode successor = root.right;
+                while(successor.left != null){
+                    successor = successor.left;
+                }
+
+                //assign successor value to node to be deleted
+                root.val = successor.val;
+
+                //delete successor node
+                root.right = deleteNode(root.right, successor.val);
             }
         }
+
         return root;
     }
 }
