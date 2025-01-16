d637e09f54fe31fa57a416c465aaeb26a25bd0bf
Bryan <BryanCrombach@gmail.com>
2025-01-16 14:47:25 +1300

    Number of Provinces (optimal solution using recursion)

--- src/Solution.java
+++ src/Solution.java
@@ -4,28 +4,25 @@ import java.util.Stack;
 
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        Stack stack = new Stack();
-        Set<Integer> visited = new HashSet<>();
-        stack.push(0);
         int provinces = 0;
-        while(visited.size() < isConnected.length) {
-            while (!stack.isEmpty()) {
-                int city = (int) stack.pop();
-                visited.add(city);
-                for (int x = 0; x < isConnected.length; x++) {
-                    if (!visited.contains(x) && isConnected[city][x] == 1) {
-                        stack.push(x);
-                    }
-                }
-            }
-            provinces++;
-            for(int i = 0; i < isConnected.length; i++) {
-                if (!visited.contains(i)) {
-                    stack.push(i);
-                    break;
-                }
+        boolean[] visited = new boolean[isConnected.length];
+
+        for (int i = 0; i < isConnected.length; i++) {
+            if(!visited[i]) {
+                provinces ++;
+                dfs(isConnected, visited, i);
             }
         }
         return provinces;
+    }
+
+    public void dfs(int[][] isConnected, boolean[] visited, int i){
+        visited[i] = true;
+
+        for(int j = 0; j < isConnected.length; j++){
+            if(isConnected[i][j] == 1 && !visited[j]){
+                dfs(isConnected, visited, j);
+            }
+        }
     }
 }
