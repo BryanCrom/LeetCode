eb399539a618c8b57ce6cba81b41aca896b0d715
Bryan <BryanCrombach@gmail.com>
2025-01-24 13:55:02 +1300

    Reorder Routes to Make All Paths Lead to the City Zero

--- Test/SolutionTest.java
+++ Test/SolutionTest.java
@@ -11,7 +11,8 @@ class SolutionTest {
     @org.junit.jupiter.api.Test
     void LeetCodeTest() {
         Solution solution = new Solution();
-        int[][] cities = new int[][]{{1,0,0},{0,1,0},{0,0,1}};
-        assertEquals(3, solution.findCircleNum(cities));
+        int n = 6;
+        int[][] connections = new int[][]{{0,1},{1,3},{2,3},{4,0},{4,5}};
+        assertEquals(3, solution.minReorder(n, connections));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,31 @@
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int provinces = 0;
-        boolean[] visited = new boolean[isConnected.length];
+    public int minReorder(int n, int[][] connections) {
 
-        for (int i = 0; i < isConnected.length; i++) {
-            if(!visited[i]) {
-                provinces ++;
-                dfs(isConnected, visited, i);
-            }
+        //convert list of edges into adjacency list with the opposing direction in negative form
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
         }
-        return provinces;
+        for (int[] connection : connections) {
+            adjList.get(connection[0]).add(connection[1]);
+            adjList.get(connection[1]).add(-connection[0]);
+        }
+        boolean[] visited = new boolean[n];
+        return dfs(adjList, visited, 0);
     }
 
-    public void dfs(int[][] isConnected, boolean[] visited, int i){
-        visited[i] = true;
-
-        for(int j = 0; j < isConnected.length; j++){
-            if(isConnected[i][j] == 1 && !visited[j]){
-                dfs(isConnected, visited, j);
+    //traverse adjacency list counting all positive numbers that we have not encountered meaning they are going the wrong direction
+    private int dfs(List<List<Integer>> adjList, boolean[] visited, int node) {
+        int ans = 0;
+        visited[node] = true;
+        for(int adj: adjList.get(node)){
+            int next = Math.abs(adj);
+            if(!visited[next]){
+                ans += dfs(adjList, visited, next) + (adj > 0 ? 1 : 0);
             }
         }
+        return ans;
     }
 }
